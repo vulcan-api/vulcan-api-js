7904bf8c6c76c82679cb7fcad5315bbbb7c37033
Kacper Stolarek <stolarek83@gmail.com>
2020-12-28 16:48:26 +0100

    Added endpoints :sparkles:

---
+++ src/hebe/endpoints.ts
@@ -0,0 +1,19 @@
+const DEVICE_REGISTER = "api/mobile/register/new";
+const STUDENT_LIST = "api/mobile/register/hebe";
+
+const DATA_ROOT = "api/mobile";
+const DATA_BY_PUPIL = "byPupil";
+const DATA_BY_PERSON = "byPerson";
+const DATA_BY_PERIOD = "byPeriod";
+const DATA_DELETED = "deleted";
+
+const DATA_INTERNAL_TIME = "internal/time";
+const DATA_LUCKY_NUMBER = "school/lucky";
+
+const DATA_EXAM = "exam";
+const DATA_GRADE = "grade";
+const DATA_GRADE_SUMMARY = "grade/summary";
+const DATA_GRADE_AVERAGE = "grade/average";
+const DATA_HOMEWORK = "homework";
+const DATA_TIMETABLE = "schedule";
+const DATA_TIMETABLE_CHANGES = "schedule/changes";
