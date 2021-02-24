800081a2d09273a66b5057e63039191d4dc7478d
Kacper Stolarek <stolarek83@gmail.com>
2021-02-24 11:17:07 +0100

    Switched to rollup :zap:

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+
+const extensions = [
+  '.js', '.jsx', '.ts', '.tsx',
+];
+
+const name = 'RollupTypeScriptBabel';
+
+export default {
+  input: './src/index.ts',
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
+  external: [...Object.keys(pkg.dependencies || {})],
+
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+
+    json(),
+    // Compile TypeScript/JavaScript files
+    babel({ extensions, include: ['src/**/*'], babelHelpers: 'runtime' }),
+  ],
+
+  output: [{
+    file: pkg.main,
+    format: 'cjs',
+  }]
+};
